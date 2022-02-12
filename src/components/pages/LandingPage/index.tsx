163402a361a147f2365d8f17c21a3c0f49915c43
Vasudeva <cbitlife@gmail.com>
2022-02-13 00:59:46 +0530

    Landing page added

--- src/components/pages/LandingPage/index.tsx
+++ src/components/pages/LandingPage/index.tsx
@@ -1,12 +1,24 @@
-import { Button, Typography, Grid } from "@mui/material";
+import { Button, Typography, Grid, Container } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 import LandingSearchBar from "../../molecules/LandingSearchBar";
+import RepublicImage from "../../../assets/images/33_Republicday.jpg";
+
+const useStyles = makeStyles({
+  root: {
+    background: `url(${RepublicImage})`,
+    height: "100vh",
+    maxWidth: "100vw !important",
+    backgroundSize: "cover",
+  },
+});
 
 const LandingPage = () => {
+  const classes = useStyles();
   return (
-    <>
+    <Container className={classes.root} fixed maxWidth="xl">
       <Typography variant="h4">Welcome to the Ahiyan Arnav</Typography>
       <LandingSearchBar />
-    </>
+    </Container>
   );
 };
 
--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,6 @@
+import { colors } from "@mui/material";
 import { orange } from "@mui/material/colors";
 import { createTheme } from "@mui/material/styles";
-import { useGlobalStyles } from "./globalStyles";
 
 let ORANGE = { ...orange, 500: "#fb6f0e" };
 const theme = createTheme({
@@ -8,6 +8,9 @@ const theme = createTheme({
     primary: {
       main: ORANGE[500],
       contrastText: "white",
+    },
+    text: {
+      primary: colors.blueGrey[200],
     },
   },
   components: {
@@ -22,6 +25,13 @@ const theme = createTheme({
           borderRadius: 0,
           height: 41,
           marginTop: 1,
+        },
+      },
+    },
+    MuiTypography: {
+      styleOverrides: {
+        h4: {
+          color: "white",
         },
       },
     },
