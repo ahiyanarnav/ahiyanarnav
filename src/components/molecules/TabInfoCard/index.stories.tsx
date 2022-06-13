6f0122ff2eb276cebc4c1335afeb3503915d702b
vasudeva-gudimalla <vasudeva.gudimalla@zemosolabs.com>
2022-06-13 22:53:13 +0530

    Technology Tab Completed

--- src/components/molecules/TabInfoCard/index.stories.tsx
+++ src/components/molecules/TabInfoCard/index.stories.tsx
@@ -1,8 +1,19 @@
 import TabInfoCard from ".";
+import IOT from "../../../assets/images/Info/Technologies/iot.png";
 
 export default {
   title: "molecules/Tab Info Card",
   component: TabInfoCard,
 };
+const description =
+  "Interest In The Internet Of Things(IoT) market is rapidly expanding as companies across multiple vertical industries recognize the need for connectivity and the potential transformation enabled through connectivity.However, IoT adoption in key segments will be limited by its complexity.";
 
-export const TabInfoCardLeft = () => <TabInfoCard imageSide="left" />;
+export const TabInfoCardLeft = () => (
+  <TabInfoCard
+    imageSide="left"
+    image={IOT}
+    key="iot"
+    label="IOT"
+    description={description}
+  />
+);
