36f00c85d02a3677c698fd1561dae36215439bb8
vasudeva-gudimalla <vasudeva.gudimalla@zemosolabs.com>
2022-06-13 17:44:17 +0530

    TabInfoCard

---
+++ src/components/molecules/TabInfoCard/index.stories.tsx
@@ -0,0 +1,8 @@
+import TabInfoCard from ".";
+
+export default {
+  title: "molecules/Tab Info Card",
+  component: TabInfoCard,
+};
+
+export const TabInfoCardLeft = () => <TabInfoCard imageSide="left" />;
--- src/components/molecules/TabsList/index.tsx
+++ src/components/molecules/TabsList/index.tsx
@@ -1,30 +1,28 @@
 /* eslint-disable react/prop-types */
 import { Tab, Tabs } from "@mui/material";
-import { useState } from "react";
 import { TabPropType } from "../../../shared/types";
 
 interface TabListsProps {
   tabsData: TabPropType[];
+  currentTab: string;
+  onHandleTabChange: (_: React.SyntheticEvent, newValue: string) => void;
 }
 
-const TabsList: React.FC<TabListsProps> = ({ tabsData }) => {
-  const [currentTab, setCurrentTab] = useState(tabsData[0].key);
-
-  const onHandleTabChange = (_: React.SyntheticEvent, newValue: string) => {
-    setCurrentTab(newValue);
-  };
-  return (
-    <Tabs value={currentTab} onChange={onHandleTabChange} variant="fullWidth">
-      {tabsData.map((tab) => (
-        <Tab
-          value={tab.key}
-          label={tab.label}
-          sx={{
-            color: "white",
-          }}
-        />
-      ))}
-    </Tabs>
-  );
-};
+const TabsList: React.FC<TabListsProps> = ({
+  tabsData,
+  currentTab,
+  onHandleTabChange,
+}) => (
+  <Tabs value={currentTab} onChange={onHandleTabChange} variant="fullWidth">
+    {tabsData.map((tab) => (
+      <Tab
+        value={tab.key}
+        label={tab.label}
+        sx={{
+          color: "white",
+        }}
+      />
+    ))}
+  </Tabs>
+);
 export default TabsList;
