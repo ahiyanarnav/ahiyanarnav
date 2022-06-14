78da0b7c28688561e7178c6d67b97c8d1ea0c616
vasudeva-gudimalla <vasudeva.gudimalla@zemosolabs.com>
2022-06-14 15:53:24 +0530

    Technology, Services and Products Tab Completed

--- src/components/molecules/InfoNavbar/index.jsx
+++ src/components/molecules/InfoNavbar/index.jsx
@@ -1,15 +1,73 @@
 import Navbar from "reactjs-navbar";
 import { Audio } from "react-loader-spinner";
 
-import Logo from "../../../assets/images/logo-main.png";
 import "./index.css";
-import navItems from "../../../shared/data/navItems";
+import {
+  faUsers,
+  faGlobe,
+  faDatabase,
+  faProjectDiagram,
+  faListSquares,
+} from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+import Logo from "../../../assets/images/logo-main.png";
+import {
+  PRODUCTS_INFO,
+  SERVICES_INFO,
+  TECHNOLOGY_INFO,
+} from "../../../shared/routes";
 
-const InfoNavbar = () => (
-  <Navbar
-    logo={Logo}
-    loader={<Audio height="25" width="25" color="grey" ariaLabel="loading" />}
-    menuItems={navItems}
-  />
-);
+const InfoNavbar = () => {
+  const navigate = useNavigate();
+  return (
+    <Navbar
+      logo={Logo}
+      loader={
+        <Audio height="25" width="25" color="grey" ariaLabel="loading.." />
+      }
+      menuItems={[
+        {
+          title: "Technology",
+          icon: faDatabase,
+          isAuth: true,
+          onClick: () => {
+            navigate(TECHNOLOGY_INFO);
+          },
+        },
+        {
+          title: "Services",
+          icon: faGlobe,
+          isAuth: true,
+          onClick: () => {
+            navigate(SERVICES_INFO);
+          },
+        },
+        {
+          title: "Products",
+          icon: faListSquares,
+          isAuth: true,
+          onClick: () => {
+            navigate(PRODUCTS_INFO);
+          },
+        },
+        {
+          title: "Clients",
+          icon: faUsers,
+          isAuth: true,
+          onClick: () => {
+            navigate(TECHNOLOGY_INFO);
+          },
+        },
+        {
+          title: "Process We Do",
+          icon: faProjectDiagram,
+          isAuth: true,
+          onClick: () => {
+            navigate(TECHNOLOGY_INFO);
+          },
+        },
+      ]}
+    />
+  );
+};
 export default InfoNavbar;
